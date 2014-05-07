0f374b1afca1ee944209859ae8f41fd1fa485a5e
Kasim Te <kasimte@gmail.com>
2014-05-07 14:14:19 -0400

    Finished plot2.

---
+++ plot2.R
@@ -0,0 +1,11 @@
+# plot2.R
+
+# Import our data.
+source("import_data.R")
+
+# Create plot2.
+png("plot2.png", width = 480, height = 480)
+plot(data$datetime, data$Global_active_power, col = "black", type = "l", xlab = "", ylab = "Global Active Power (kilowatts)")
+dev.off()
+
+
